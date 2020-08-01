05f9e0a579703cca2e760741ca65f1f313f7bb9a
root <lexuanhieu131297@gmail.com>
2020-08-01 18:51:00 +0000

    editing metrics

--- utils/custom_metric.py
+++ utils/custom_metric.py
@@ -3,7 +3,7 @@ import numpy as np
 def iou_numpy(outputs: np.array, labels: np.array,smooth=0.000001,thresholding=False):
     SMOOTH = smooth
     outputs = outputs.squeeze(1)
-    
+    labels = labels.squeeze(1)
     intersection = (outputs & labels).sum((1, 2))
     union = (outputs | labels).sum((1, 2))
     
